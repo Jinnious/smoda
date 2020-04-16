2bb9eabc5ea52cb5972bac741c9c3be40a6e6872
Juhye Kim <jujubebe@Juhyes-iMac.local>
2020-04-16 15:06:27 -0700

    css change

--- stories/StartButton.stories.js
+++ stories/StartButton.stories.js
@@ -11,12 +11,14 @@ export const MyStartButton = () => <StartButton />;
 
 export const PageWithButtons = () => <div>
     <div>
-    <StartButton text="Start"/>
+    <StartButton text="Start" bgcolor="#c8553D" />
     </div>
     <div>
-    <StartButton text="Basic"/> 
+    <StartButton text="Basic" bgcolor="#F28F3c" /> 
     </div>
     <div>
-    <StartButton text="Advanced" />
+    <StartButton text="Advanced" bgcolor="#707070" />
     </div>
 </div>
+
+
